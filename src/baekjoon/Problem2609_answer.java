9f3e48e321bb612c31d9b1c012bed1f141c6bcb1
jaesung <peter.yu.0402@gmail.com>
2025-05-12 12:35:39 +0900

    test(최대공약수와 최소공배수): problem2609
    유클리도 호제법 재귀방식 활용

---
+++ src/baekjoon/Problem2609_answer.java
@@ -0,0 +1,33 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Problem2609_answer {
+    // 유클리드 호제법 (재귀 방식)
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out))
+                ) {
+            // 입력 받기
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int a = Integer.parseInt(stringTokenizer.nextToken());
+            int b = Integer.parseInt(stringTokenizer.nextToken());
+
+            // 최대공약수, 최소공배수 구하기
+            int gcd = gcd(a, b);
+            int lcm = a * b / gcd;
+
+            // 정답 출력
+            bufferedWriter.write(gcd + "\n" + lcm);
+        }
+    }
+}
